f6abe1524399f1057e435be7deba9da8e657d772
arturpezda <atipezda@gmail.com>
2020-11-07 12:27:00 +0100

    UPDATE: docker-helper: - version prefix - rename

--- docker-helper.sh
+++ docker-helper.sh
@@ -1,16 +1,19 @@
+#!/bin/bash
 dirName="${PWD##*/}"
+prefix="vestra_"
 composePath="";
 command="";
 execCommand=""
+versionPrefix=""
 
 clear
 echo "Select version: "
 select yn in "docker-run" "codeception-run" "codeception-run-tests" "prod"; do
     case $yn in
-        docker-run ) composePath="docker-run/docker-compose.yml"; break;;
-        codeception-run ) composePath="docker-codeception-run/docker-compose.yml"; break;;
-        codeception-run-tests ) composePath="docker-codeception-run/docker-compose.test.yml"; break;;
-        prod ) composePath="docker-compose.yml"; break;;
+        docker-run ) composePath="docker-run/docker-compose.yml"; versionPrefix="docker_run_"; break;;
+        codeception-run ) composePath="docker-codeception-run/docker-compose.yml"; versionPrefix="docker_codeception_run_"; break;;
+        codeception-run-tests ) composePath="docker-codeception-run/docker-compose.test.yml"; versionPrefix="docker_codeception_run_tests_"; break;;
+        prod ) composePath="docker-compose.yml";  versionPrefix="prod_"; break;;
     esac
 done
 clear
@@ -29,7 +32,5 @@ select yn in "up" "down" "build" "up --build" "stop" "start"; do
         stop ) command="stop"; break;;
         start ) command="start"; break;;
     esac
-done
-execCommand="docker-compose -p $dirName -f $composePath $command";
 echo $execCommand
 eval $execCommand
